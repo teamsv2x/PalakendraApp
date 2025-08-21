49989636134da0e7eecd82d05a0cb10b79b00fa2
VineethGoud712 <vineethganagoni@gmail.com>
2025-08-21 16:05:04 -0500

    added comment in API

--- src/main/java/com/palakendra/palakendra/common/ApiError.java
+++ src/main/java/com/palakendra/palakendra/common/ApiError.java
@@ -1,5 +1,7 @@
 package com.palakendra.palakendra.common;
 
 import java.time.Instant; import java.util.Map;
-public record ApiError(Instant timestamp, int status, String error, String message, String path, Map<String,Object> details) {}
+public record ApiError(Instant timestamp, int status, String error, String message, String path, Map<String,Object> details) {
+    //code here
+}
 
